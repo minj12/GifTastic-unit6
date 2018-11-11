25e1bb076b3c96c7b81bd5c694a960021aebd359
min <mjnorthwesternwork1121@gmail.com>
2018-11-11 00:10:09 -0600

    better filing.Bit of BOOTSTRAP. Refined and shorter code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,135 +1,66 @@
-//Setting Initial Array Values
-var gifys = ["Batman", "Guardians of the Galaxy", "Star-Trek", "Star-Wars", "Futurama", "Flash Gordan", "Wander Over Yonder", "Development", "Galaxy Quest", "Interstellar"];
-
-function displayGifInfo(gifName) {
-
-  //Main variables
-  var gify = $(this).attr("data-name");
-  var limit = 10;
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gify + "&limit="+ limit + "&api_key=ED5M7CisPjLAtbyt9UiIJpQdgioBnDSz&limit=10";
-
-  //AJAX uses "Get" to bring data from var queryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function (response) {
-
-    //console logging
-    console.log(queryURL);
-    console.log(response); 
-
-    //var assignments from html doc
-    var gifyDiv = $("<div class='gify'>");
-    
-    var results = response.data;
-
-
-    //Clears out previous giphy set before adding a new set
-    $("#gif-view").empty();
-    for (var i = 0; i < gifys.length; i++) {
-
-      //Filtering Guidance rateing//NO ADULT CONTENT 
-      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-
-        // Generating div with class "item"
-        var gifyDiv = $("<div class='item'>");
-
-        //Generating a div to hold the giphys
-        var gifyDiv = $("<div>");
-
-        //Storing rating response
-        var rating = response.data.rating;
-
-        //Fetching URL for image
-        var imgURL = response.rating;
-
-        //Generating <p> and rating
-        var p = $("<p>").text("Rating: " + results[i].rating );
-
-        //Generating image tag
-        var gify = $("<img>");
-
-        //Defining src attribute of the images pulled
-        gify.attr("src", results[i].images.fixed_height.url);
-        gify.attr("src",)
-
-        //Appending rating to giphy
-        gifyDiv.append(p);
-        gifyDiv.append(gify);
-
-        //Setting src and URL attributes to giphy
-        var image = $("<img>").attr("src", imgURL);
-
-        //Appending the giphy
-        gifyDiv.append(image);
-
-        //Prepending new giphys above previosly called giphys
-        $("#gify-view").prepend(gifyDiv);
-      };
-    };
-  });
-};
-
-//Calling renderButtons function
-function renderButtons() {
-
-  //Prevents repeated buttons
-  $("#buttons-view").empty();
-
-  //For Loop
-  for (var i = 0; i < gifys.length; i++) {
-
-    //Button
-    var a = $("<button>");
-    //Adding class of gify
-    a.addClass("gify");
-    //Adding data-attribute
-    a.attr("data-name", gifys[i]);
-    //Adding button text
-    a.text(gifys[i]);
-    //Appending the button to HTML
-    $("#buttons-view").append(a);
-  };
-};
-
-//On.click function; prevents duplicatation of initial buttons
-$("#add-gify").on("click", function (event) {
-  event.preventDefault();
-
-  //Stores user input from the textbox
-  var gify = $("#gify-input").val().trim();
-
-  //Removes previous giphys on.click
-  $("gif").empty();
-
-  //Adds Users input from the textbox to array
-  gifys.push(gify);
-
-  //Calls renderButtons function for User input buttons
-  renderButtons();
+$(document).ready(function () {
+  // Main global variables
+var topics = ["Batman", "Guardians of the Galaxy", "Star-Trek", "Star-Wars", "Futurama", "Flash Gordan", "Wander Over Yonder", "Development", "Galaxy Quest", "Interstellar"];
+var siten;
+  // creating buttons for topics array
+  for (var i = 0; i < topics.length; i++) {
+      var button = $("<button>");
+      button.addClass("btn btn-secondary");
+      button.text(topics[i]);
+      button.attr("data-meme", topics[i]);
+      $("#memeButtons").append(button);
+  }
+  // function for submit button
+  $("#addMeme").on("click", function (event) {
+   // preventing the button from trying to submit the form
+      event.preventDefault();
+      var inputMeme = $("#meme-input").val().trim();
+      var newButton = $("<button>");
+      newButton.addClass("btn btn-secondary");
+      newButton.text(inputMeme);
+      newButton.attr("data-meme", inputMeme);
+      $("#memeButtons").append(newButton);
+  })
+  // event listener for meme buttons
+  $("#memeButtons").on("click", "button", function () {
+      $("#gifsHere").empty();
+      siten = $(this).attr("data-meme");
+      // storing giphy API URL for random memes
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + siten + "&api_key=zZpHwgeLyNsagu20ptH3a5OTh0gx5742&limit=10";
+      // performing an AJAX request with the query URL
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+      })
+          // after data comes back from the request
+          .done(function (response) {
+              var results = response.data;
+              for (var i = 0; i < results.length; i++) {
+                  var memeDiv = $("<div>");
+                  memeDiv.addClass("memeDiv");
+                  var rating = $("<p>").text("Rating: " + results[i].rating);
+                  var memeImage = $("<img>");
+                  memeImage.addClass("gif");
+                  // storing still and animated gifs 
+                  memeImage.attr("src", results[i].images.fixed_height_still.url);
+                  memeImage.attr("data-still", results[i].images.fixed_height_still.url);
+                  memeImage.attr("data-animate", results[i].images.fixed_height.url);
+                  memeImage.attr("data-state", "still")
+                  memeDiv.append(rating);
+                  memeDiv.append(memeImage);
+                  $("#gifsHere").append(memeDiv);
+              }
+              // click function for gifs 
+              $(".gif").on("click", function () {
+                  var state = $(this).attr("data-state");
+                  if (state === "still") {
+                      $(this).attr("src", $(this).attr("data-animate"));
+                      $(this).attr("data-state", "animate");
+                  } else {
+                      $(this).attr("src", $(this).attr("data-still"));
+                      $(this).attr("data-state", "still");
+                  }
+              })
+          })
+  })
 });
-
-function imageChangeState() {          
-
-    var state = $(this).attr("data-state");
-    var animateImage = $(this).attr("data-animate");
-    var stillImage = $(this).attr("data-still");
-
-    if(state == "still") {
-        $(this).attr("src", animateImage);
-        $(this).attr("data-state", "animate");
-    }
-
-    else if(state == "animate") {
-        $(this).attr("src", stillImage);
-        $(this).attr("data-state", "still");
-    }   
-}
-
-
-
-//Adds a click event listener to elements with a class of "gify"
-$(document).on("click", ".gify", displayGifInfo);
-
-//Calls the renderButtons function for the intial buttons as defined in the array
-renderButtons();
